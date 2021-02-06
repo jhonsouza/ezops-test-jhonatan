2de8b07e7b1e0823e0ea236c2d88a47e9c6358b6
Jhonatan Siqueira <jhonatan.siqueira@al.resilia.com.br>
2021-02-06 11:46:31 -0300

    init project simple chat

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const cors = require('cors')
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const mongoose = require('mongoose');
+
+const uri = "mongodb+srv://jhonatan:tfdfsxzW7eHpjBbZ@cluster0.zftne.mongodb.net/ezops?retryWrites=true&w=majority";
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname))
+
+const Message = mongoose.model('Message', {
+  name: String,
+  message: String
+})
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true}, (err) =>{
+    var connectionStatus = !err ? 'Success': 'Error Connecting to database';
+console.log(connectionStatus);
+});
+
+
+
+
+app.get('/messages', async (req, res) => {
+  console.log("Get no banco")
+  const result = await Message.find()
+  Message.fi
+  return res.send(result)
+})
+
+
+
+app.get('/messages/:user', (req, res) => {
+  var user = req.params.user
+  Message.find({name: user},(err, messages)=> {
+    res.send(messages);
+  })
+})
+
+
+app.post('/messages', async (req, res) => {
+  const result = await Message.create(req.body)
+  console.log(result)
+  return res.json(result)
+})
+
+
+
+io.on('connection', () =>{
+  console.log('a user is connected')
+})
+
+app.use(cors())
+
+const server = http.listen(3000, () => {
+  console.log('server is running on port', server.address().port);
+});
