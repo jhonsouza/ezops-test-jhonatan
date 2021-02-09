5e030ca7ec7c666c488a0f0babf0391e2712bc82
Jhonatan Siqueira <jhonatan.siqueira@al.resilia.com.br>
2021-02-08 22:49:31 -0300

    update after file

--- script/after_install.sh
+++ script/after_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
+sudo mkdir /home/ec2-user/ezops-test-jhonatan/logs
+sudo chown ec2-user:ec2-user -R /home/ec2-user/ezops-test-jhonatan
 cd /home/ec2-user/ezops-test-jhonatan
 sudo npm install
-sudo mkdir logs
