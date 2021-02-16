c48c9f73bc8376db986f7cd7e1bb725c529bae4b
Jhonatan Siqueira <jhonatan.siqueira@al.resilia.com.br>
2021-02-16 16:54:06 -0300

    update application stop

--- script/application_stop.sh
+++ script/application_stop.sh
@@ -1,6 +1,5 @@
 #!bin/bash
 
 cd /home/ec2-user/ezops-test-jhonatan/
-docker container stop simplechat
-docker container rm simplechat
+docker container rm ezops-test-jhonatan_node_1 -f
 #forever stop src/server.js > logs/stop_app.log
